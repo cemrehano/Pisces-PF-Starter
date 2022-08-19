5396ac22338afc585aa466c2a5c962639f861ec9
cemrehano <111056538+cemrehano@users.noreply.github.com>
2022-08-19 17:25:32 +0300

    Update pf_start_install.sh

--- pf_start_install.sh
+++ pf_start_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo wget -O /root/pf-start.sh https://raw.githubusercontent.com/Pisces-PF-Starter/blob/main/pf-start.sh; chmod a+x /root/pf-start.sh
+sudo wget -O /root/pf-start.sh https://raw.githubusercontent.com/cemrehano/Pisces-PF-Starter/main/pf-start.sh; chmod a+x /root/pf-start.sh
 { crontab -l; echo "@reboot sh /root/pf-start.sh"; } | crontab -
